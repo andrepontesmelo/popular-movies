157da5099f46a9fcb683e762657af2a1e198b8f3
andrepontesmelo <andrepontes@gmail.com>
2015-08-04 06:45:32 -0300

    Second blank activity.

--- PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/MainActivity.java
@@ -1,35 +1,16 @@
 package com.pontes.andre.popularmovies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-
-                new Intent(createDisplayContext());
-
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        setContentView(R.layout.activity_main_activity_fragment);
     }
 
     @Override
@@ -40,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id = item.getItemId();
 
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
---
+++ PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/MainActivityFragment.java
@@ -0,0 +1,41 @@
+package com.pontes.andre.popularmovies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_main_activity, container, false);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+
+        gridview.setAdapter(new ImageAdapter(view.getContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                Intent details = new Intent(getActivity(), DetailActivity.class);
+                startActivity(details);
+            }
+        });
+
+        return view;
+    }
+}
