b768a7bd669916d8ddda320ed8d0db5f367ff012
andrepontesmelo <andrepontes@gmail.com>
2015-08-06 22:14:51 -0300

    showing movie details @ details activity

--- PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/DetailActivity.java
@@ -2,8 +2,6 @@ package com.pontes.andre.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -20,18 +18,5 @@ public class DetailActivity extends AppCompatActivity {
     private Movie getMovie()
     {
         return (Movie) this.getIntent().getSerializableExtra("movie");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
