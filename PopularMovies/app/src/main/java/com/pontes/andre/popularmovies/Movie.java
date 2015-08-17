2d326e89c1b9611162aee806ede8f842a0bf73ea
andrepontesmelo <andrepontes@gmail.com>
2015-08-17 13:35:41 -0300

    using parcelable instead of serializable for efficiency.

--- PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/Movie.java
+++ PopularMovies/app/src/main/java/com/pontes/andre/popularmovies/Movie.java
@@ -1,63 +1,88 @@
 package com.pontes.andre.popularmovies;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Movie implements Serializable {
+public class Movie implements Parcelable {
 
     private String title;
-    private Date releaseDate;
     private String posterUrl;
-
-    private float voteAvgInTen;
-
     private String synopsis;
+    private float voteAvgInTen;
+    private Date releaseDate;
+
 
     public String getSynopsis() {
         return synopsis;
     }
-
     public void setSynopsis(String synopsis) {
         this.synopsis = synopsis;
     }
-
     public float getVoteAvgInTen() {
         return voteAvgInTen;
     }
-
     public float getVoteAvgInFive() {
         return voteAvgInTen / 2;
     }
-
     public void setVoteAvgInTen(float voteAvgInTen) {
         this.voteAvgInTen = voteAvgInTen;
     }
-
     public String getPosterUrl() {
         return posterUrl;
     }
-
     public void setPosterUrl(String posterUrl) {
         this.posterUrl = posterUrl;
     }
-
     public Date getReleaseDate() {
         return releaseDate;
     }
-
     public void setReleaseDate(Date releaseDate) {
         this.releaseDate = releaseDate;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getCompletePosterUrl() {
         return "http://image.tmdb.org/t/p/w185" + posterUrl;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(title);
+        out.writeString(posterUrl);
+        out.writeString(synopsis);
+        out.writeFloat(voteAvgInTen);
+        out.writeSerializable(releaseDate);
+    }
+
+    public Movie()
+    {
+    }
+
+    private Movie(Parcel in) {
+        title = in.readString();
+        posterUrl = in.readString();
+        synopsis = in.readString();
+        voteAvgInTen = in.readFloat();
+        releaseDate = (Date) in.readSerializable();
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
